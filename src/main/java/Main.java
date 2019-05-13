9e136418291b2d0dc59375b4e06d4778f0a25247
SevyConst <lopatko@protonmail.ch>
2019-05-13 12:52:10 +0300

    reduce usless packages

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ForProperties config = new ForProperties();
+        if (!config.loadProperties()) {
+            return;
+        }
+
+        Client client = new Client(config.ip, config.port);
+        System.out.println("Done!");
     }
 }
