da410bf2a86e95f9fed4e952799b323f7c92a85f
SevyConst <lopatko@protonmail.ch>
2019-05-13 12:56:35 +0300

    Add some output to console for clarity

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,9 @@ public class Main {
         if (!config.loadProperties()) {
             return;
         }
-
+        System.out.println("file with config has been read successful!");
+        System.out.println("------------------------------------------");
+        System.out.println("start connecting!");
         Client client = new Client(config.ip, config.port);
         System.out.println("Done!");
     }
